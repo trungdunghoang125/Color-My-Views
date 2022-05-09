625aa8bc61fb34fd943edd1c759fd8515aed160c
trungdunghoang <trungdunghoang125@gmail.com>
2022-05-09 22:56:52 +0700

    Change color of view to random color

--- app/src/main/java/com/trungdunghoang125/colormyviews/MainActivity.kt
+++ app/src/main/java/com/trungdunghoang125/colormyviews/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.trungdunghoang125.colormyviews
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,13 +13,13 @@ class MainActivity : AppCompatActivity() {
         setListener()
     }
 
-    fun makeColored(view : View) {
+    private fun makeColored(view : View) {
         when (view.id) {
-            R.id.box_one_text -> view.setBackgroundResource(android.R.color.holo_red_light)
-            R.id.box_two_text -> view.setBackgroundResource(android.R.color.holo_orange_light)
-            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_purple)
+            R.id.box_one_text -> view.setBackgroundResource(randomColor())
+            R.id.box_two_text -> view.setBackgroundResource(randomColor())
+            R.id.box_three_text -> view.setBackgroundResource(randomColor())
+            R.id.box_four_text -> view.setBackgroundResource(randomColor())
+            R.id.box_five_text -> view.setBackgroundResource(randomColor())
 
             R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
             R.id.yellow_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
@@ -46,6 +45,18 @@ class MainActivity : AppCompatActivity() {
                                                 redButton, yellowButton, greenButton)
         for (item in clickableView) {
             item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun randomColor(): Int {
+        return when ((1..6).random()) {
+            1 -> R.color.lavender_pink
+            2 -> R.color.dark_pastel_purple
+            3 -> R.color.topaz
+            4 -> R.color.little_boy_blue
+            5 -> R.color.baby_blue_eyes
+
+            else -> R.color.platinum
         }
     }
 }
